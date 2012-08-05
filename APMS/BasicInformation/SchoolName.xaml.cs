b028a4817bf0752b54162a4a9c50c1ac89150021
JackJCSN <qq541921209@gmail.com>
2012-08-06 00:09:01 +0800

    增加数据集绑定和业务逻辑

--- APMS/BasicInformation/SchoolName.xaml.cs
+++ APMS/BasicInformation/SchoolName.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DataProvider;
 
 namespace APMS.BasicInformation
 {
@@ -18,9 +19,101 @@ namespace APMS.BasicInformation
     /// </summary>
     public partial class SchoolName : Window
     {
+        App app = (App)App.Current;
+        private Authentication Auth;
+
         public SchoolName()
         {
+            Auth = app.Auth;
             InitializeComponent();
+            GetList();
+        }
+
+        private void GetList()
+        {
+            School[]  s = School.GetSchools(Auth);
+
+            CollegelistBox.ItemsSource = s;
+        }
+
+        private void Exiting(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            GetList();
+        }
+
+        private void Insert(object sender, RoutedEventArgs e)
+        {
+            if (CollegeName_textBox.Text == "")
+            {
+                return;
+            }
+            try
+            {
+                School.Insert(CollegeName_textBox.Text, Auth);
+            }
+            catch (NoPermissionException ex)
+            {
+                MessageBox.Show(this, "权限不足！\r\n" + ex.Message, "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "系统错误！\r\n" + ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            GetList();
+        }
+
+        private void Modify(object sender, RoutedEventArgs e)
+        {
+            School s = null;
+            try
+            {
+                s =  (School)CollegelistBox.SelectedItem;
+                if (s != null)
+                {
+                    s.Name = CollegeName_textBox.Text;
+                    s.Update(Auth);
+                }
+            }
+            catch (NoPermissionException ex)
+            {
+                MessageBox.Show(this, "权限不足！\r\n" + ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "系统错误！\r\n" + ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            GetList();
+            if (s != null)
+            {
+                CollegelistBox.SelectedValue = s.Name;
+            }
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            School s = null;
+            try
+            {
+                s =  (School)CollegelistBox.SelectedItem;
+                if (s != null)
+                {
+                    s.Delete(Auth);
+                }
+            }
+            catch (NoPermissionException ex)
+            {
+                MessageBox.Show(this, "权限不足！\r\n" + ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "系统错误！\r\n" + ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            GetList();
         }
     }
 }
